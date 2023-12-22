6f9ac2e1037d7da7a3e2b4c07a892d402a8d9edf
Harsimranji-singh <hsdosanjh81468@gmail.com>
2023-12-21 22:03:39 -0500

    interest_middle

--- src/components/Interest.js
+++ src/components/Interest.js
@@ -1,5 +1,36 @@
 import React from "react";
-
+const interestsData = [
+   {
+      icon: "https://placekitten.com/64/64",
+      title: "MERN Stack",
+      description:
+         "Full stack development using MongoDB, Express.js, React, and Node.js.",
+   },
+   {
+      icon: "https://placebear.com/64/64",
+      title: "GraphQL",
+      description:
+         "Query language for APIs, often used with modern web applications.",
+   },
+   {
+      icon: "https://placekitten.com/64/64",
+      title: "React Native",
+      description:
+         "Build mobile applications using React for iOS and Android platforms.",
+   },
+   {
+      icon: "https://placebeard.it/64/64", // Placeholder image of a beard (64x64 pixels)
+      title: "Express.js",
+      description:
+         "Web application framework for Node.js, designed for building web and mobile apps.",
+   },
+   {
+      icon: "https://placecage.it/64/64", // Placeholder image of Nicolas Cage (64x64 pixels)
+      title: "Node.js",
+      description:
+         "JavaScript runtime for server-side development, built on Chrome's V8 JavaScript engine.",
+   },
+];
 const Interest = () => {
    let content = (
       <div className="interest__main">
@@ -12,12 +43,19 @@ const Interest = () => {
             </p>
          </div>
          <div className="interest__content">
-            <div className="interest__card">hello</div>
-            <div className="interest__card">hello</div>
-            <div className="interest__card">hello</div>
-            <div className="interest__card">hello</div>
-            <div className="interest__card">hello</div>
-            <div className="interest__card">hello</div>
+            {interestsData.map((item, key) => (
+               <div className="interest__card">
+                  <div className="interest__icon">
+                     <img src={item.icon} alt={item.title} />
+                  </div>
+                  <div className="interest__item__heading">
+                     <h3>{item.title}</h3>
+                  </div>
+                  <div className="interest__description">
+                     <p>{item.description}</p>
+                  </div>
+               </div>
+            ))}
          </div>
       </div>
    );
